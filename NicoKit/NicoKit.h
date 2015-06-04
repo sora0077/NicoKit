650de56eee818e87fbe8429f0000021127e817ee
林達也 <t_hayashi@hayashitatsuya-no-Mac-Pro.local>
2015-06-05 02:25:00 +0900

    Initial Commit

---
+++ NicoKit/NicoKit.h
@@ -0,0 +1,19 @@
+//
+//  NicoKit.h
+//  NicoKit
+//
+//  Created by 林達也 on 2015/06/05.
+//  Copyright (c) 2015年 林達也. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NicoKit.
+FOUNDATION_EXPORT double NicoKitVersionNumber;
+
+//! Project version string for NicoKit.
+FOUNDATION_EXPORT const unsigned char NicoKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NicoKit/PublicHeader.h>
+
+
